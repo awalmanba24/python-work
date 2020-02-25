8574b0cd4faec35c583c63ab718e0be349780f5f
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:44:02 -0500

    ass1

---
+++ string_manipulation.py
@@ -0,0 +1,14 @@
+first_name=input("enter you first name please")
+last_name=input("enter you last name please")
+print("so you first name is",first_name,"and your last name is", last_name)
+print("thank you")
+print(first_name.title())
+#upper and lower case
+mixed_string = "GoOdByE"
+print(mixed_string.lower())
+print(mixed_string.upper())
+integer_number=3
+print("here is the number as a float: %.2f" %integer_number)
+integer_number+6 
+float_number=5
+print("Here is first variable passed into format {:06.2f} and here is the second {:.2f}".format(integer_number, float_number))
--- variables.py
+++ variables.py
@@ -1,3 +1,11 @@
+name="awal"
+name_l='olanrewaju'
+print("my name is",name,name_l)
 todo=float(1.0)
 age=22
 print('next year i  would be',todo+age)
+print("my name is",name,name_l,"and i would be", todo+age,"next year")
+user=input("what is your name?")
+user_age=int(input("what your age?"))
+print("so your name is",user,"and your age is",user_age,"cool")
+print("your now my friend",user)
